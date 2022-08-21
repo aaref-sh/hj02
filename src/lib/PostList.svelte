4e3ba7a72fe0f423c8956f7f7f209bdce48f494f
aaref shami <40777651+aaref-sh@users.noreply.github.com>
2022-08-21 19:33:34 +0300

    implement signin/register - postlist at homepage

--- src/lib/PostList.svelte
+++ src/lib/PostList.svelte
@@ -2,6 +2,22 @@
 
 <script>
 
+let postss = [];
+    var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+    };
+
+    fetch("https://localhost:5000/api/Users/PostsGetAll", requestOptions)
+    .then(response => response.text())
+    .then(result =>{
+        postss = [];
+        postss = JSON.parse(result);
+    } )
+    .catch(error => console.log('error', error));
+
+
+
     export let tags;
 
     let posts = [
@@ -29,25 +45,28 @@
 </script>
 
 <div class="list-container m-1 sm:m-2">
-    {#each posts as post}
-        <div class="p-1 mt-2 border border-blue-200/70 rounded shadow-sm hover:shadow hover:bg-cyan-50/40 transition-all flex">
+    {#if postss.length == 0}
+        لا يوجد نتائج تطابق البحث
+    {/if}
+    {#each postss as post}
+        <div class="p-1 mt-2 border border-blue-200/70 rounded shadow-sm hover:shadow hover:bg-cyan-50/30 transition-all flex">
             <a href="./{post.id}" class="h-ful w-full flex-auto">
                 <h3 class="list-title px-5 py-2 text-blue-1000 text-lg md:text-2xl">
                     {post.title}
                 </h3>
                 <hr class="border-t border-sky-300/50 ml-1">
                 <div class="grid grid-cols-1 sm:grid-cols-2 pt-2  px-3">
-                    <a href="./users/{post.publisher_id}" class="post-header-item">
+                    <a href="./users/{post.userId}" class="post-header-item">
                         <span class="material-symbols-outlined">
                             person
                         </span> &nbsp;
-                        {post.publisher}
+                        {post.userName}
                     </a>
                     <div class="post-header-item">
                         <span class="material-symbols-outlined">
                             schedule
                         </span> &nbsp;
-                        {post.date}
+                        {post.created}
                     </div>
                     <div class="post-header-item">
                         <span class="material-symbols-outlined">
@@ -55,11 +74,19 @@
                         </span> &nbsp;
                         {post.views}
                     </div>
+                    <div class="post-header-item">
+                        <span class="material-symbols-outlined">
+                            thumb_up
+                        </span> &nbsp;
+                        {post.likes}
+                    </div>
                 </div>
             </a>
-            <div class="post-image flex-none h-24 w-24 std-border bg-cover rounded-md" style="background-image: url({post.image});">
-
-            </div>
+            {#if post.picture!=""}
+                <div class="post-image flex-none h-24 w-24 std-border bg-cover rounded-md" 
+                    style="background-image: url({post.picture});">
+                </div>
+            {/if}
         </div>
     {/each}
 </div>
